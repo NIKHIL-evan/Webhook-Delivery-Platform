d06fa676094e60c2c36bf3741a63ffdc86c31c9a
NIKHIL-evan <ynikhil.1207@gmail.com>
2026-05-08 17:01:22 +0000

    feat: implement phase 0 endpoints with synchronous delivery

--- app/routers/attempts.py
+++ app/routers/attempts.py
@@ -1,2 +1,23 @@
+import uuid
 from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import Depends
+from app.database import get_db
+from app.models import DeliveryAttempts, Events
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
 router = APIRouter()
+
+@router.get("/events/{event_id}/delivery_attempts")
+async def fetch_attempts(event_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    stm = select(Events).where(Events.event_id == event_id)
+    result = await db.execute(stm)
+    event = result.scalar_one_or_none()
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    else:
+        stmt = select(DeliveryAttempts).where(DeliveryAttempts.event_id == event_id)
+        result = await db.execute(stmt)
+        attempts = result.scalars().all()
+        return attempts
--- app/routers/endpoints.py
+++ app/routers/endpoints.py
@@ -1,2 +1,23 @@
 from fastapi import APIRouter
+from app.models import Endpoints
+from pydantic import BaseModel
+from app.database import get_db
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 router = APIRouter()
+
+class EndpointCreate(BaseModel):
+    url: str 
+
+@router.post("/endpoints")
+async def register_url(body: EndpointCreate, db: AsyncSession = Depends(get_db)):
+    endpoint = Endpoints(url=body.url)
+    db.add(endpoint)
+    await db.commit()
+    await db.refresh(endpoint)
+    return {
+    "url_id": str(endpoint.url_id),
+    "url": endpoint.url,
+    "created_at": str(endpoint.created_at)
+    }
