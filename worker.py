4886529f6f09e61aa1d26185f178c5e66b8374e4
NIKHIL-evan <ynikhil.1207@gmail.com>
2026-05-13 09:01:10 +0000

    phase 2: add error handling, debug logging to worker and retry scheduler

--- worker.py
+++ worker.py
@@ -9,143 +9,129 @@ import random
 
 async def worker_loop():
     while True:
+        try:
+            response = await redis_client.xreadgroup(
+                groupname="delivery_workers",
+                consumername="worker-1",
+                streams={"webhook_events": ">"},
+                count=1,
+                block=2000
+                )
+            if not response:
+                continue
 
-        response = await redis_client.xreadgroup(
-            groupname="delivery_workers",
-            consumername="worker-1",
-            streams={"webhook_events": ">"},
-            count=1,
-            block=2000
-            )
-        if not response:
-            continue
+            messages = response[0][1]
+            message_id, fields = messages[0]
+            event_id = fields["event_id"]
+            endpoint_id = fields["endpoint_id"]
+            
+            print(f"worker: processing event {event_id}", flush=True)
 
-        messages = response[0][1]
-        message_id, fields = messages[0]
-        event_id = fields["event_id"]
-        endpoint_id = fields["endpoint_id"]
-        
+            async with AsyncSessionLocal() as session:
+                stm = select(Endpoints).where(Endpoints.url_id == endpoint_id)
+                result = await session.execute(stm)
+                endpoint = result.scalar_one_or_none()
+                destination_url = endpoint.url
 
-        async with AsyncSessionLocal() as session:
-            stm = select(Endpoints).where(Endpoints.url_id == endpoint_id)
-            result = await session.execute(stm)
-            endpoint = result.scalar_one_or_none()
-            destination_url = endpoint.url
+                stm2 = select(Events).where(Events.event_id == event_id)
+                result2 = await session.execute(stm2)
+                event = result2.scalar_one_or_none()
+                payload = event.payload
+                attempt_count = event.attempt_count + 1
 
-            stm2 = select(Events).where(Events.event_id == event_id)
-            result2 = await session.execute(stm2)
-            event = result2.scalar_one_or_none()
-            payload = event.payload
-            attempt_count = event.attempt_count + 1
+                try:
+                    async with httpx.AsyncClient() as client:
+                        response = await client.post(
+                            destination_url,
+                            json=payload,
+                            timeout=10.0  #to catch httpx.TimeoutException if destination url hangs
+                        )
 
-            try:
-                async with httpx.AsyncClient() as client:
-                    response = await client.post(
-                        destination_url,
-                        json=payload,
-                        timeout=10.0  #to catch httpx.TimeoutException if destination url hangs
-                    )
-                    if response.status_code < 300:
-                        attempt = DeliveryAttempts(event_id=event_id, 
-                                                    attempt_number=attempt_count,
-                                                    status="success",
-                                                    response_code= response.status_code)
-                        session.add(attempt)
+                        print(f"worker: status {response.status_code}", flush=True)
 
-                        event.status = "delivered"
+                        if response.status_code < 300:
+                            attempt = DeliveryAttempts(event_id=event_id, 
+                                                        attempt_number=attempt_count,
+                                                        status="success",
+                                                        response_code= response.status_code)
+                            session.add(attempt)
+
+                            event.status = "delivered"
+                            event.attempt_count = attempt_count
+                            session.add(event)
+                            await session.commit()
+
+                            # Acknowledge to stream
+                            await redis_client.xack("webhook_events", "delivery_workers", message_id)
+
+                        else:
+                            await redis_client.xack("webhook_events", "delivery_workers", message_id)
+
+                            if attempt_count >= 5:
+                                event.status = "dead"
+                                event.next_retry_at = None
+                                event.attempt_count = attempt_count
+                                session.add(event)
+                            else:
+                                event.status = "failed"
+                                event.next_retry_at = datetime.now(timezone.utc) + timedelta(seconds=(2 ** attempt_count) + random.randint(0, 3))
+                                event.attempt_count = attempt_count
+                                session.add(event)
+
+                            f_attempt = DeliveryAttempts(event_id=event_id,
+                                                            attempt_number=attempt_count,
+                                                            status="failed",
+                                                            response_code=response.status_code)
+                            session.add(f_attempt)
+                            await session.commit()
+
+                except  httpx.RequestError:
+                    await redis_client.xack("webhook_events", "delivery_workers", message_id)
+
+                    if attempt_count >= 5:
+                        event.status = "dead"
+                        event.next_retry_at = None
                         event.attempt_count = attempt_count
                         session.add(event)
-                        await session.commit()
-
-                        # Acknowledge to stream
-                        await redis_client.xack("webhook_events", "delivery_workers", message_id)
 
                     else:
-                        await redis_client.xack("webhook_events", "delivery_workers", message_id)
+                        event.status = "failed"
+                        event.next_retry_at = datetime.now(timezone.utc) + timedelta(seconds=(2 ** attempt_count) + random.randint(0, 3))
+                        event.attempt_count = attempt_count
+                        session.add(event)
 
-                        if attempt_count >= 5:
-                            event.status = "dead"
-                            event.next_retry_at = None
-                            event.attempt_count = attempt_count
-                            session.add(event)
-                        else:
-                            event.status = "failed"
-                            event.next_retry_at = datetime.now(timezone.utc) + timedelta(seconds=(2 ** attempt_count) + random.randint(0, 3))
-                            event.attempt_count = attempt_count
-                            session.add(event)
+                                    
+                    f2_attempt = DeliveryAttempts(event_id=event_id,
+                                                attempt_number=attempt_count,
+                                                status="failed",
+                                                response_code=None)
+                    session.add(f2_attempt)
+                    await session.commit()
 
-                        f_attempt = DeliveryAttempts(event_id=event_id,
-                                                        attempt_number=attempt_count,
-                                                        status="failed",
-                                                        response_code=response.status_code)
-                        session.add(f_attempt)
-                        await session.commit()
-
-            except  httpx.RequestError:
-                await redis_client.xack("webhook_events", "delivery_workers", message_id)
-
-                if attempt_count >= 5:
-                    event.status = "dead"
-                    event.next_retry_at = None
-                    event.attempt_count = attempt_count
-                    session.add(event)
-
-                else:
-                    event.status = "failed"
-                    event.next_retry_at = datetime.now(timezone.utc) + timedelta(seconds=(2 ** attempt_count) + random.randint(0, 3))
-                    event.attempt_count = attempt_count
-                    session.add(event)
-
-                                
-                f2_attempt = DeliveryAttempts(event_id=event_id,
-                                            attempt_number=attempt_count,
-                                            status="failed",
-                                            response_code=None)
-                session.add(f2_attempt)
-                await session.commit()
-
+        except Exception as e:
+            print(f"worker_loop crashed: {e}", flush=True)
+            await asyncio.sleep(1)
+            
 async def retry_loop():
     while True:
         await asyncio.sleep(5)
-        async with AsyncSessionLocal() as session:
-            stm = select(Events).where(Events.status == "failed", Events.next_retry_at <= datetime.now(timezone.utc), Events.attempt_count < 5)
-            result = await session.execute(stm)
-            events = result.scalars().all()
-            for event in events:
-                url_id = event.url_id
-                event_id = event.event_id
-                await redis_client.xadd("webhook_events", {"event_id": str(event_id), "endpoint_id": str(url_id)})
-                event.status = "pending"
-                session.add(event)
-            await session.commit()
+        try:
+            async with AsyncSessionLocal() as session:
+                stm = select(Events).where(Events.status == "failed", Events.next_retry_at <= datetime.now(timezone.utc), Events.attempt_count < 5)
+                result = await session.execute(stm)
+                events = result.scalars().all()
+                for event in events:
+                    url_id = event.url_id
+                    event_id = event.event_id
+                    print(f"retry_scheduler: re-enqueuing {event_id}", flush=True)
+                    await redis_client.xadd("webhook_events", {"event_id": str(event_id), "endpoint_id": str(url_id)})
+                    event.status = "pending"
+                    session.add(event)
+                await session.commit()
+
+        except Exception as e:
+            print(f"worker_loop error: {e}", flush=True)
+            await asyncio.sleep(1)
             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     asyncio.run(worker_loop())
